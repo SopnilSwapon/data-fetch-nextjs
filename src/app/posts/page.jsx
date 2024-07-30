3f33d22b2d831d393a71f857df8d7e40a7a5e9c3
SopnilSwapon <sopnilswapon1@gmail.com>
2024-07-30 14:22:27 +0600

    added posts page

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
-const PostsPage = () => {
+const PostsPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=30');
+    const posts = await res.json();
+    console.log(posts);
+
     return (
-        <div>
-           Posts coming sooon!!!!!! 
+        <>
+        <h2 className='text-4xl font-bold text-center mt-5'>Our All Posts </h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
+           {
+            posts.map(post =>
+                 <div key={post.id} className='bg-gray-500 rounded p-2'>
+                    <h2 className='text-xl font-bold'>{post.title}</h2>
+                    <p>{post.body}</p>
+                 </div>)
+           }
         </div>
+        </>
     );
 };
 
