8b78ef992ff6aef3278354e8a8f0942eab1be5c0
SopnilSwapon <sopnilswapon1@gmail.com>
2024-07-30 13:55:45 +0600

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    const navLinks =
+        [
+            {
+                title: 'HOME',
+                path: '/home'
+            },
+            {
+                title: 'About',
+                path: '/about'
+            },
+            {
+                title: 'Contact',
+                path: '/contact'
+            },
+            {
+                title: 'Services',
+                path: '/services'
+            },
+            {
+                title: 'Posts',
+                path: '/posts'
+            }
+        ]
+    return (
+        <div className='flex justify-between bg-green-500 font-bold p-3'>
+            <h2 className='text-2xl'>LOGO</h2>
+            <ul className='flex gap-5'>
+             {
+                navLinks.map(link => <Link href={link.path} key={link.path}>{link.title}</Link>)
+             }
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
