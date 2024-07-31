84e400446e3794ebc97a72185f8fb78cec583eef
SopnilSwapon <sopnilswapon1@gmail.com>
2024-07-31 17:11:37 +0600

    added earch posted dynamic tilte by using genarateMetadata funcition

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,10 +1,21 @@
 import { ClientPageRoot } from 'next/dist/client/components/client-page';
 import React from 'react';
 
+export const generateMetadata = async ({params}) =>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+    const data = await res.json();
+    return {
+        title: `${data.title}`,
+        description: data.body
+    }
+}
+
 const postDetailsPage = async ({params}) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
     const {title, body} = await res.json();
-    console.log(params.id)
+    console.log(params.id
+    
+    )
     return (
         <div>
             <h2 className='text-xl mb-1'>{title}</h2>
