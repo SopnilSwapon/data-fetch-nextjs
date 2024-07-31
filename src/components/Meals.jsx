eb3df5d3dae4e6513253bd0c6a1c785301cc5661
SopnilSwapon <sopnilswapon1@gmail.com>
2024-07-31 14:17:28 +0600

    added meals by searching meals on the meals page

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,25 +1,72 @@
 "use client"
 import { Button, TextField } from '@mui/material';
 import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Margin } from '@mui/icons-material';
 
 const Meals = () => {
-    const [searchText, setSearchText] = useState('')
-    const handleSearch = event => {
-        console.log("from f" , searchText)
-}
-const handleSetSearchText = (e) => {
-    setSearchText(e.target.value)
-}
+    const [searchText, setSearchText] = useState('rice');
+    const [meals, setMeals] = useState([]);
+    const handleSearch = async () => {
+        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`);
+        const data = await res.json();
+        setMeals(data.meals)
+    }
+
+
+    const handleSetSearchText = (e) => {
+        setSearchText(e.target.value)
+    }
+    console.log(searchText)
     return (
-        <div className='flex justify-center gap-3 mt-4'>
-        <TextField  sx={{
-     display: 'flex',
-     justifyContent: 'center',
-     alignItems: 'center',
-   }}  
-   onChange={handleSetSearchText}
-   halfWidth name='search' id="outlined-secondary" variant="outlined" placeholder='search'/>
-   <Button onClick={handleSearch} variant="outlined">Search</Button>
+        <div>
+            <div className='flex justify-center gap-3 mt-4'>
+                <TextField sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+                    onChange={handleSetSearchText}
+                    halfWidth name='search' id="outlined-secondary" variant="outlined" placeholder='search' />
+                <Button onClick={handleSearch} variant="outlined">Search</Button>
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    meals?.length ? <>
+                        {
+                            meals?.map(meal =>
+                                <div key={meal.idMeal}>
+                                    <Card sx={{ m: 2 }}>
+                                        <CardMedia
+                                            sx={{ height: 140 }}
+                                            image={meal.strMealThumb}
+                                            title="green iguana"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="div">
+                                                {meal.strMeal}
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary">
+                                                {meal.strInstructions.slice(0, 500)}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small">details</Button>
+                                            <Button size="small">Buy</Button>
+                                        </CardActions>
+                                    </Card>
+                                </div>)
+                        }
+                    </>
+                        :
+                        <Typography className='col-span-3' variant='h2' align='center' sx={{mt: 5}}>Not Found</Typography>
+                }
+            </div>
         </div>
     );
 };
